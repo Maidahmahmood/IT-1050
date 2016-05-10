448d708441e879b9fc87a279f0793e9fd8fda6fa
Maidah <maidah.mahmood@gmail.com>
2016-05-10 18:55:36 -0400

    Final Assignment

---
+++ Final Exam/Final Exam/Elevator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final_Exam
+{
+    public class Elevator
+    {
+        private double MaxWeight;
+        private Passenger[] Occupants;
+
+        public Elevator(int maxOccupants, double maxWeight)
+        {
+            Occupants = new Passenger[maxOccupants];
+            MaxWeight = maxWeight;
+        }
+
+        public void AddOccupant(Passenger passenger, int index)
+        {
+            Occupants[index] = passenger;
+        }
+        public double GetCurrentWeight()
+        {
+            double totalWeight = 0;
+            for(int i = 0; i < Occupants.Length; i++)
+            {
+                totalWeight += Occupants[i].GetWeight;
+            }
+            return totalWeight;
+        }
+        public bool IsOverMaxCapacity()
+        {
+            return GetCurrentWeight() > MaxWeight;
+        }
+    }
+}
---
+++ Final Exam/Final Exam/Passenger.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final_Exam
+{
+    public class Passenger
+    {
+        private string Name;
+        private double Weight;
+
+        public Passenger(string name, double weight)
+        {
+            Name = name;
+            Weight = weight;
+        }
+
+        public string GetName
+        {
+            get { return Name; }
+        }
+        public double GetWeight
+        {
+            get { return Weight; }
+        }
+    }
+}
